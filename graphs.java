a44ff45c35f8c5db5369f78df64cacafaffbede0
rayz236 <xzhang236@dons.usfca.edu>
2026-04-06 23:55:47 -0700

    Implement graph cloning using DFS

--- graphs.java
+++ graphs.java
@@ -1,1 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
 
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+
+class Solution {
+    HashMap<Node, Node> map = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null){
+            return null;
+        }
+        if (map.containsKey(node)) {
+            return map.get(node);
+        }
+        Node newNode = new Node(node.val);
+        map.put(node, newNode);
+        for (Node neighbor : node.neighbors) {
+            newNode.neighbors.add(cloneGraph(neighbor));
+        }
+        return newNode;
+    }
+}
